a1afa9c6fb036e440f59dbd8456b5fbf4e1f63fa
JDReeves86 <Reeves.Jacobd@gmail.com>
2023-03-28 12:34:51 -0700

    hiding credentials

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,29 @@
 //connects to SQL database specified in .env file.
-require('dotenv').config();
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} 
-else {
-    sequelize = new Sequelize (
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        });
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.AWSDB_URL,
+      dialect: "mysql",
+      port: 3306,
+      dialectOptions: {
+        ssl: "Amazon RDS",
+      },
+      logging: console.log,
+      maxConcurrentQueries: 100,
+      pool: { maxConnections: 5, maxIdleTime: 30 },
+      language: "en",
+    }
+  );
 }
 
 module.exports = sequelize;
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,22 +1,23 @@
 const User = require('../models/User')
+require('dotenv').config()
 
 const userData = [
     {
         user_name: 'Lord Humungus',
-        email: 'Lord@Humungus.com',
-        password: 'password',
+        email: process.env.USER1EMAIL,
+        password: process.env.USER1PASS,
         power_level: 9800
     },
     {
         user_name: 'Jedediah',
-        email: 'gyro@captain.com',
-        password: 'password',
+        email: process.env.USER2EMAIL,
+        password: process.env.USER2PASS,
         power_level: 4200
     },
     {
         user_name: 'MaxRockatansky',
-        email: 'mad@max.com',
-        password: 'password',
+        email: process.env.USER3EMAIL,
+        password: process.env.USER3PASS,
         power_level: 2600
     },
 ];
